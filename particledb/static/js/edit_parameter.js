f866c3747ca7e00e074b2bd9567bb55100d6470e
Valentin Voigt <mail@valentinvoigt.info>
2015-05-02 01:33:25 +0200

    Added security confirmation to parameter deletion

--- particledb/static/js/edit_parameter.js
+++ particledb/static/js/edit_parameter.js
@@ -1,20 +1,21 @@
 $(function() {
     $('.remove-paramter').click(function() {
         var id = $(this).attr('data-id');
-        
-        var jqxhr = $.ajax({
-            url: js_globals.parameter_remove_url,
-            method: "POST",
-            data: {id: id},
-            dataType: "json",
-        }).fail(function(jqXHR, textStatus) {
-            // TODO
-            alert("failed :(");
-        }).done(function(data) {
-            $('tr[data-row-param-id='+id+']').fadeOut('fast', function() { $(this).remove(); });
-            /* $("tr:not(.hidden)").each(function (index) {
-                $(this).toggleClass("stripe", !!(index & 1));
-            }); */
+        bootbox.confirm({
+            message: "Do you really want to delete this parameter?",
+            size: 'small',
+            callback: function(result) {
+                if (result) {
+                    var jqxhr = $.ajax({
+                        url: js_globals.parameter_remove_url,
+                        method: "POST",
+                        data: {id: id},
+                        dataType: "json",
+                    }).done(function(data) {
+                        $('tr[data-row-param-id='+id+']').fadeOut('fast', function() { $(this).remove(); });
+                    });
+                }
+            },
         });
     });
 });
