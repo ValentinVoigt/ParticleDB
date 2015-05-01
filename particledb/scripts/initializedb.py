4b13af8b448ed152443c32b6ed3bf460e2d19c58
Valentin Voigt <mail@valentinvoigt.info>
2015-05-01 17:41:04 +0200

    Removed default data from repository

--- particledb/scripts/initializedb.py
+++ particledb/scripts/initializedb.py
@@ -20,49 +20,6 @@ def usage(argv):
           '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
 
-def create_models():
-    ## Maxim
-    maxim_logo = Image(path='manufacturers/maxim.svg', alt="Maxim logo")
-    yield maxim_logo
-    maxim = Manufacturer(name='Maxim', logo_image=maxim_logo)
-    yield maxim
-    
-    ## TI
-    ti_logo = Image(path='manufacturers/ti.svg', alt="Texas Instruments logo")
-    yield maxim_logo
-    ti = Manufacturer(name='Texas Instruments', logo_image=ti_logo)
-    yield ti
-
-    ## Atmel
-    atmel_logo = Image(path='manufacturers/atmel.svg', alt="Atmel logo")
-    yield atmel_logo
-    atmel = Manufacturer(name='Atmel', logo_image=atmel_logo)
-    yield atmel
-
-    ## Cypress
-    cypress_logo = Image(path='manufacturers/cypress.svg', alt="Cypress logo")
-    yield cypress_logo
-    cypress = Manufacturer(name='Cypress', logo_image=cypress_logo)
-    yield cypress
-    
-    ## Lite-On
-    liteon_logo = Image(path='manufacturers/liteon.png', alt="Lite-On logo")
-    yield liteon_logo
-    liteon = Manufacturer(name='Lite-On', logo_image=liteon_logo)
-    yield liteon
-    
-    ## Parts
-    yield Part(mpn="TEST123", description="Simple test component", manufacturer=maxim)
-    yield Part(mpn="TEST345", description="Another test component", manufacturer=ti)
-    
-    ## Packages
-    for i in [2, 3]:
-        yield Package(name="TO-220", pins=i)
-        yield Package(name="TO-92", pins=i)
-    
-    for i in range(4, 28+1, 2):
-        yield Package(name="DIP-%i" % i, pins=i)
-    
 def main(argv=sys.argv):
     if len(argv) < 2:
         usage(argv)
@@ -73,6 +30,3 @@ def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    with transaction.manager:
-        for model in create_models():
-            DBSession.add(model)
