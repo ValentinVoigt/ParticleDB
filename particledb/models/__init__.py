cf8a4837ac4777f554041e194ba1a294dcfdbb10
Valentin Voigt <mail@valentinvoigt.info>
2015-05-04 02:48:45 +0200

    Added base model with convenience functions

--- particledb/models/__init__.py
+++ particledb/models/__init__.py
@@ -1,10 +1,32 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.inspection import inspect
 from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-Base = declarative_base()
+DeclarativeBase = declarative_base()
 
+class Base(DeclarativeBase):
+    """ Base model for all models.
+    """
+    __abstract__ = True
+    
+    @classmethod
+    def get(cls, *args):
+        """ Returns object by primary ID.
+        Returns None if object is not found.
+        """
+        primary_keys = [i.key for i in inspect(cls).primary_key]
+        filter_ = dict(zip(primary_keys, args))
+        return DBSession.query(cls).filter_by(**filter_).first()
+
+    @classmethod
+    def get_by(cls, **kwargs):
+        """ Returns the first object by given filter.
+        Returns None if object is not found.
+        """
+        return DBSession.query(cls).filter_by(**kwargs).first()
+        
 from .image import Image
 from .manufacturer import Manufacturer
 from .package import Package
