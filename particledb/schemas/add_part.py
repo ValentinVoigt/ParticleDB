cba40e955d049aca58cb070dff022fec69c010f0
Valentin Voigt <mail@valentinvoigt.info>
2015-05-04 04:25:25 +0200

    Added .strip() to new parts schema

--- particledb/schemas/add_part.py
+++ particledb/schemas/add_part.py
@@ -7,7 +7,7 @@ from ..models import DBSession, Part
 class UniqueMPN(formencode.FancyValidator):
 
     def _convert_to_python(self, value, state):
-        query = DBSession.query(Part.id).filter(Part.mpn==value)
+        query = DBSession.query(Part.id).filter(Part.mpn==value.strip())
         if query.count() > 0:
             raise formencode.Invalid('That MPN already exists', value, state)
         return value
@@ -15,7 +15,7 @@ class UniqueMPN(formencode.FancyValidator):
 class AddPartSchema(formencode.Schema):
 
     mpn = formencode.All(
-        validators.String(not_empty=True),
+        validators.String(not_empty=True, strip=True),
         UniqueMPN(),
     )
     description = validators.String()
