7616a9dc84abf8f8a06674b18029e0498d87de46
Valentin Voigt <mail@valentinvoigt.info>
2017-03-03 02:56:35 +0100

    Fixed MPNs with slashes

--- particledb/__init__.py
+++ particledb/__init__.py
@@ -21,7 +21,7 @@ def main(global_config, **settings):
     config.add_route('list_parts', '/list/parts/{page}')
     config.add_route('list_packages', '/list/packages/{page}')
     config.add_route('list_manufacturers', '/list/manufacturers/{page}')
-    config.add_route('part', '/parts/{part_mpn}')
+    config.add_route('part', '/parts/{part_mpn:.*}')
     config.add_route('add_part', '/add/part')
     config.add_route('storage', '/storage')
 
